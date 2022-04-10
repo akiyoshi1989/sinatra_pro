1cf02aaa335cacaadfc304b411e59097c5a9c08b
akiyoshi1989 <a.yoshimura1989@gmail.com>
2022-04-10 09:29:02 +0000

    handle select-box

--- headless/scrape_pol.rb
+++ headless/scrape_pol.rb
@@ -7,9 +7,6 @@ class Scrape
   include Capybara::DSL
 
   def initialize()
-    # Capybara.register_driver :poltergeist_debug do |app|
-    #   Capybara::Poltergeist::Driver.new(app, :inspector => true)
-    # end
     Capybara.register_driver :poltergeist do |app|
         Capybara::Poltergeist::Driver.new(app, {
                           js_errors: false,
@@ -21,23 +18,26 @@ class Scrape
                                     '--ssl-protocol=any']})
       end
     Capybara.default_driver = :poltergeist
-    Capybara.javascript_driver = :poltergeist
+    # Capybara.javascript_driver = :poltergeist
   end
 
-  def visit_site
+  def visit_input_page
     #ユーザエージェント設定（必要に応じて）
     page.driver.headers = { "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36" }
     # 対象画面
     visit('http://localhost:4567/input')
     #スクリーンショットで保存
-    page.save_screenshot('screenshot/1.png', :full => false)
+    page.save_screenshot(rtn_ss_name('scrape'), :full => false)
     page.first('input').native.send_key('Akito Yoshimura')
+    page.select('30', :from => 'age')
+
     page.find('textarea').native.send_key('free comment')
     
-    page.save_screenshot('screenshot/2.png', :full => false)
+    page.save_screenshot(rtn_ss_name('scrape'))
     
     page.find('input.btn').click
-    page.save_screenshot('screenshot/3.png', :full => true)
+    
+    page.save_screenshot(rtn_ss_name('scrape'))
     #within(:xpath, "//*[@id='toipcsfb']/div[1]/ul[1]") do
     #Nokogirオブジェクトの作成
     # doc = Nokogiri::HTML.parse(page.html)
@@ -46,5 +46,21 @@ class Scrape
   end
 end
 
+def rtn_ss_name(base_file_name)
+  dir_name = './screenshot'
+
+  unless Dir.exist?(dir_name) then
+    Dir.mkdir(dir_name)
+  end
+  i = 1
+  loop{
+    file_name = "#{dir_name}/#{base_file_name}_#{i}.png"
+    puts file_name
+    unless File.exist?(file_name)
+      return file_name
+    end
+    i += 1
+  }
+end
 scrape = Scrape.new
-scrape.visit_site
+scrape.visit_input_page
