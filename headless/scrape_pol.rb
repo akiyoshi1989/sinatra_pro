55d3ef89e713eba5cb19cb56df0a461bfcd902e6
akiyoshi1989 <a.yoshimura1989@gmail.com>
2022-04-10 15:26:44 +0000

    add handle longSS

--- headless/scrape_pol.rb
+++ headless/scrape_pol.rb
@@ -3,7 +3,7 @@ require 'capybara/dsl'
 require 'capybara/poltergeist'
 
 class Scrape
-  #DSLのスコープを別けないと警告がでます
+
   include Capybara::DSL
 
   def initialize()
@@ -11,56 +11,65 @@ class Scrape
         Capybara::Poltergeist::Driver.new(app, {
                           js_errors: false,
                           timeout: 5000,
-                        #   debug: true,
+                        #   debug: true, コメント解除でコンソール上に処理状況が吐き出される
                           phantomjs_options: [
                                     '--load-images=no',
                                     '--ignore-ssl-errors=yes',
                                     '--ssl-protocol=any']})
       end
     Capybara.default_driver = :poltergeist
-    # Capybara.javascript_driver = :poltergeist
   end
+
 
   def visit_input_page
     #ユーザエージェント設定（必要に応じて）
     page.driver.headers = { "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36" }
-    # 対象画面
+    # 対象画面を開く
     visit('http://localhost:4567/input')
-    #スクリーンショットで保存
-    page.save_screenshot(rtn_ss_name('scrape'), :full => false)
-    page.first('input').native.send_key('Akito Yoshimura')
-    page.select('30', :from => 'age')
 
-    page.find('textarea').native.send_key('free comment')
+    #スクリーンショットで保存
+    page.save_screenshot(rtn_ss_name('scrape'))
+
+    # 入力フォームに値をセット
+    page.first('input').native.send_key('akiyoshi')
+    page.select('30', :from => 'age')
+    page.find('textarea').native.send_key('細かい操作は対象画面の構造が変わると、動かなくなります（泣）')
     
     page.save_screenshot(rtn_ss_name('scrape'))
     
     page.find('input.btn').click
     
     page.save_screenshot(rtn_ss_name('scrape'))
-    #within(:xpath, "//*[@id='toipcsfb']/div[1]/ul[1]") do
-    #Nokogirオブジェクトの作成
-    # doc = Nokogiri::HTML.parse(page.html)
-    # puts doc.textarea
-    # puts doc
+
+    # page.find('input.btn').clickと指定の仕方は違うが、やってることは同じ。
+    page.click_button('submit')
+    page.save_screenshot(rtn_ss_name('scrape'), :full => false)
+    page.save_screenshot(rtn_ss_name('scrape'), :full => true)
+
+    # NokogiriでHTMLをパースする。今回は使っていないが、本格的にスクレイピングする場合は本命。
+    doc = Nokogiri::HTML.parse(page.html)
+    puts doc.title
   end
 end
 
+# スクリーンショット名（ファイル名重複時の上書き防止）を作る
 def rtn_ss_name(base_file_name)
   dir_name = './screenshot'
 
   unless Dir.exist?(dir_name) then
     Dir.mkdir(dir_name)
   end
+  
   i = 1
   loop{
     file_name = "#{dir_name}/#{base_file_name}_#{i}.png"
-    puts file_name
     unless File.exist?(file_name)
       return file_name
     end
     i += 1
   }
 end
+
+# 実行部
 scrape = Scrape.new
 scrape.visit_input_page
