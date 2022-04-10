6a398bcfae0c7eda255edc3b691b872e995a9987
akiyoshi <a.yoshimura1989@example.com>
2022-04-10 00:24:47 +0000

    snatra app first commit

---
+++ sinatra_pro/app/app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+set :environment, :production
+
+get '/' do
+  'indexpage'
+end
+
+get '/input' do
+  haml :input_form
+end
+
+post '/confirm' do
+  @sender_name = params[:sender_name]
+  @free_form = params[:free_form]
+  haml :confirm
+end
+
+post '/complete' do
+  if params[:btn_value] == "submit"
+    haml :complete
+  else
+    haml :input_form
+  end
+end
